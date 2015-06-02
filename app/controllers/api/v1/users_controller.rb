7b7b553716f6c8fc99d827218f648c0f763b23dd
jaketrent <trent.jake@gmail.com>
2015-06-01 20:55:34 -0600

    add cors headers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class UsersController < ApplicationController
+    class UsersController < BaseController
       def index
         @users = User.all
         render json: @users
